2b79be0f6cccbb29ef22c7d6087dd6d9afe4ccff
bala-amypo <bala@amypo.in>
2025-12-18 08:54:43 +0000

    User commit

--- src/main/java/com/example/demo/service/UserService.java
+++ src/main/java/com/example/demo/service/UserService.java
@@ -1,1 +1,20 @@
 package com.example.demo.service;
+
+
+
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository
+    userRepository;
+
+    public User findByUsername(String username ) {
+        return userRepository.findByUsername(username);
+    }
+
+    public User save(User user) {
+        return useRepository.save
+    }
+}
