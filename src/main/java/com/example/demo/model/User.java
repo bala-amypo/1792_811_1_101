fc0cbf711663cce21f812c817d8bfe26a56c9e42
bala-amypo <bala@amypo.in>
2025-12-18 08:31:09 +0000

    User commit

--- src/main/java/com/example/demo/model/User.java
+++ src/main/java/com/example/demo/model/User.java
@@ -24,5 +24,19 @@ public User(String username, String password, String email) {
 }
 
 public Long getId() {
-    return id
+    return id;
 }
+
+public void setId(Long id) {
+    this.id = id;
+}
+
+public String getUsername() {
+    return username;
+}
+
+public void setUsername(String username) {
+    this.username = username;
+}
+
+public String 
