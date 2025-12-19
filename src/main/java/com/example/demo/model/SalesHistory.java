d091f3e925de4ba1c961c25e727eb8f2db598e95
bala-amypo <bala@amypo.in>
2025-12-19 05:58:02 +0000

    User commit

---
+++ src/main/java/com/example/demo/model/SalesHistory.java
@@ -0,0 +1,12 @@
+pacakage com.example.inventory.entity;
+
+import jakarta.prsistence.*;
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "sales_history")
+public class SalesHistory {
+
+    @Id
+    @GeneratedValue(stra)
+}
