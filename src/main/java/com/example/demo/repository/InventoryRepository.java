cba68ded33475550a1eeb6d5acf3c58b01f6012f
bala-amypo <bala@amypo.in>
2025-12-22 08:31:47 +0000

    User commit

---
+++ src/main/java/com/example/demo/repository/InventoryRepository.java
@@ -0,0 +1,13 @@
+package com.example.demo.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+public interface InventoryRepository
+        extends JpaRepository<Inventory, Long> {
+    Optional<Inventory> findByProductName(String productName);
+}
