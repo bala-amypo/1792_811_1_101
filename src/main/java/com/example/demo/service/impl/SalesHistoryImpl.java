36b141d6a02d9c5fd50aa2c16c2374e4ec0bdc4a
bala-amypo <bala@amypo.in>
2025-12-22 09:33:32 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/SalesHistoryImpl.java
+++ src/main/java/com/example/demo/service/impl/SalesHistoryImpl.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 
 @Service
-public class SalesHistorySerImpl implements SalesHistoryService {
+public class SalesHistoryServiceImpl implements SalesHistoryService {
 
     private final SalesHistoryRepository repository;
 
