60a5b0f2d55f44706ddcc0baf14cff1612d8ffc4
bala-amypo <bala@amypo.in>
2025-12-23 09:14:42 +0000

    User commit

--- src/main/java/com/example/demo/entity/Warehouse.java
+++ src/main/java/com/example/demo/entity/Warehouse.java
@@ -6,14 +6,14 @@ import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "warehouses")
-public class WareHouse {
+public class Warehouse {
 
     @Id
     private Long id;
     private String name;
     private String location;
 
-    public WareHouse() {}
+    public Warehouse() {}
 
     public Long getId() { return id; }
     public void setId(Long id) { this.id = id; }
