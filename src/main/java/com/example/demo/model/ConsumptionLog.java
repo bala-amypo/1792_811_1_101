1a1b2cf11bbab4fb49c35d44d519a438887cf25b
bala-amypo <bala@amypo.in>
2025-12-26 09:31:57 +0000

    User commit

---
+++ src/main/java/com/example/demo/model/ConsumptionLog.java
@@ -0,0 +1,22 @@
+package com.example.demo.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+import java.time.LocalDate;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ConsumptionLog {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Integer consumedQuantity;
+    private LocalDate consumedDate;
+
+    @ManyToOne
+    private StockRecord stockRecord;
+}
---
+++ src/main/java/com/example/demo/model/StockRecord.java
@@ -0,0 +1,25 @@
+package com.example.demo.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class StockRecord {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    private Product product;
+
+    @ManyToOne
+    private Warehouse warehouse;
+
+    private Integer currentQuantity;
+    private LocalDateTime lastUpdated;
+}
