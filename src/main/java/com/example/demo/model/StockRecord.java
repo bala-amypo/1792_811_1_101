2ee726e7dc0d99862d1da36ce1f993cee2fe1d96
bala-amypo <bala@amypo.in>
2025-12-28 08:32:27 +0000

    User commit

--- src/main/java/com/example/demo/model/StockRecord.java
+++ src/main/java/com/example/demo/model/StockRecord.java
@@ -1,21 +1,5 @@
 package com.example.demo.model;
 
-import lombok.*;
-import java.time.LocalDateTime;
-
-@Data
-@Builder
-@NoArgsConstructor
-@AllArgsConstructor
-public class StockRecord {
-    private Long id;
-    private Product product;
-    private Warehouse warehouse;
-    private Integer currentQuantity;
-    private LocalDateTime lastUpdated;
-}
-package com.example.demo.model;
-
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
 import jakarta.persistence.Id;
